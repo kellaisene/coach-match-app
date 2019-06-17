c2f236814b79f0e0fd1ae769b9827d9dd17d6fc8
Keller Johnson <kjlt4412@gmail.com>
2019-06-17 11:02:21 -0600

    Getting basic stuff in there as placeholders

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/header.css';
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="header-container">
+				<div>Here's the Header</div>
+			</div>
+		);
+	}
+}
+
+export default Header;
---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/homePage.css';
+
+class HomePage extends Component {
+	render() {
+		return (
+			<div className="home-page-container">
+				<div>Here's the HomePage</div>
+			</div>
+		);
+	}
+}
+
+export default HomePage;
