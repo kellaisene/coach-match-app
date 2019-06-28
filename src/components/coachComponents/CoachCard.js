c4a67fd1a730867645586c5fa473d3f1c2fd7135
Keller Johnson <kjlt4412@gmail.com>
2019-06-28 12:20:30 -0600

    playing with styling

--- src/components/coachComponents/CoachCard.js
+++ src/components/coachComponents/CoachCard.js
@@ -5,25 +5,35 @@ export default function CoachCard(props) {
 	console.log('Props', props);
 	return (
 		<div className="coach-card-container">
-			<div className="data">
+			{/* <div className="data">
 				<div className="name">Name:</div>
+			</div> */}
+			<div className="content">
 				<div className="margining">{props.firstName}</div>
 				<div className="margining">{props.lastName}</div>
 			</div>
-			<div className="data">
+			{/* <div className="data2">
 				<div className="name">Summary:</div>
+			</div> */}
+			<div className="content">
 				<div className="margining">{props.summary}</div>
 			</div>
-			<div className="data">
+			{/* <div className="data3">
 				<div className="name">Location:</div>
+			</div> */}
+			<div className="content">
 				<div className="margining">{props.location}</div>
 			</div>
-			<div className="data">
+			{/* <div className="data4">
 				<div className="name">Job Title:</div>
+			</div> */}
+			<div className="content">
 				<div className="margining">{props.jobTitle}</div>
 			</div>
-			<div className="data">
+			{/* <div className="data5">
 				<div className="name">Hourly Rate:</div>
+			</div> */}
+			<div className="content">
 				<div className="margining">{props.hourlyRate}</div>
 			</div>
 		</div>
