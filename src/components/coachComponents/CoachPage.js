1484bcae52316f50b354e560831f8b6e3a2239d7
Keller Johnson <kjlt4412@gmail.com>
2019-06-29 12:30:41 -0600

    Added pagination

--- src/components/coachComponents/CoachPage.js
+++ src/components/coachComponents/CoachPage.js
@@ -8,9 +8,24 @@ class CoachPage extends Component {
 	constructor() {
 		super();
 		this.state = {
-			dummyData: data
+			dummyData: data,
+			currentPage: 1,
+			itemsPerPage: 5
 		};
+		this.loadMore = this.loadMore.bind(this);
 	}
+
+	loadMore() {
+		this.setState((prev) => {
+			return { itemsPerPage: prev.itemsPerPage + 5 };
+		});
+	}
+
+	// handleClick(e) {
+	// 	this.setState({
+	// 		currentPage: Number(e.target.id)
+	// 	})
+	// }
 
 	displayData = () => {
 		return data.map((data) => {
@@ -29,11 +44,23 @@ class CoachPage extends Component {
 		});
 	};
 	render() {
+		const { currentPage, itemsPerPage } = this.state;
+		const indexOfLastItem = currentPage * itemsPerPage; // Start of pagination
+		const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+		const currentItems = this.displayData().slice(indexOfFirstItem, indexOfLastItem);
+		const renderItems = currentItems.map((item, index) => {
+			return <li key={index}>{item}</li>;
+		});
 		console.log('Mock Data', this.state.dummyData);
 		return (
 			<main className="coach-page-container">
 				<header className="coach-page-header">Find a coach!</header>
-				<div>{this.displayData()}</div>
+				<div className="items">{renderItems}</div>
+				{this.state.itemsPerPage < data.length && (
+					<button onClick={this.loadMore} type="button">
+						Load More
+					</button>
+				)}
 			</main>
 		);
 	}
