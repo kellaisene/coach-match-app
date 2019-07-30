4dd1953d4ca82257be74594d74496d60efd868e9
Keller Johnson <kjlt4412@gmail.com>
2019-07-30 17:05:31 -0600

    added router but getting errors

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import './styles/App.css';
 
 import Header from '../src/components/Header';
 import HomePage from './components/HomePage';
@@ -8,9 +9,11 @@ import SignUp from './components/SignUp';
 
 export default (
 	<Switch>
-		<Header className="header" />
-		<Route exact path="/" component={HomePage} />
-		<Route path="/coachpage" component={CoachPage} />
-		<Route path="/signup" component={SignUp} />
+		<div className="App">
+			<Header className="header" />
+			<Route exact path="/" component={HomePage} />
+			<Route path="/coachpage" component={CoachPage} />
+			<Route path="/signup" component={SignUp} />
+		</div>
 	</Switch>
 );
